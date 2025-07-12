2c4ae392e54c69c9041a747f604aa6161ff5e01b
Anas <mohdanasali3136@gamil.com>
2025-07-12 20:33:47 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Todo List
+This is my mini project. It is a basic Todo list.
+
+## Description 
+A simple and responsive To-Do List web application that helps users keep track of tasks.
+User can add our task which need to performed and we can also delte or remove any task .
+User can also  edit their task from a list 
+
+## Features
+1) Add task button to add different task
+2) Delte button to remove any task
+3) Edit button to change your task in case you entered a wrong task
+
+## Technologies Used
+1) HTML
+2) CSS(vanilla)
+3) Java-Script (basic Dom manipulation)
+
---
+++ app.js
@@ -0,0 +1,85 @@
+let input = document.querySelector("#task");
+let taskbtn = document.querySelector(".add-task");
+let list = document.querySelector(".task-list");
+let editTask = document.querySelector(".edit-task");
+editTask.style.display="none";
+
+taskbtn.addEventListener("click", function (){
+      let task = document.createElement("li");
+     task.classList.add("li");
+    
+     let delbtn = document.createElement("i");
+     let editbtn = document.createElement("i");
+     let span = document.createElement("span");
+     span.classList.add("task-text");
+     span.innerText = input.value;
+     delbtn.classList.add("fa-solid","fa-trash");
+     editbtn.classList.add("fa-regular","fa-pen-to-square")
+     delbtn.setAttribute("id","del");
+     editbtn.setAttribute("id","edit");
+     let checkbox = document.createElement("input");
+     checkbox.setAttribute("type","checkbox");
+     checkbox.setAttribute("id","done")
+ 
+    task.appendChild(checkbox);
+    task.appendChild(span);
+    task.appendChild(editbtn);
+     task.appendChild(delbtn);
+      list.appendChild(task);
+      console.log("task added");
+      input.value = "";
+});
+
+list.addEventListener("click", function (ele){
+    console.log(ele.target);
+    console.log("btn was clicked");
+    // list.removeChild(ele.target.parentElement);
+    if(ele.target.id=="del"){
+        let par = ele.target.parentElement;
+        par.remove();
+    }
+    if(ele.target.type == "checkbox"){
+        let par = ele.target.parentElement;
+        if(ele.target.checked == true){
+            par.classList.add("complete");
+          let icon =  ele.target.nextElementSibling.nextElementSibling ;
+           icon.style.display = "none";
+           icon.nextElementSibling.style.display = "none";
+            console.dir(par);
+        }
+        else{
+            par.classList.remove("complete");
+           let icon =  ele.target.nextElementSibling.nextElementSibling ;
+           icon.style.display = "";
+           icon.nextElementSibling.style.display = "";
+        }
+    }
+    if(ele.target.id=="edit"){
+        let task = ele.target.previousElementSibling;
+        taskbtn.style.display = "none";
+        editTask.style.display="";
+        console.log(task);
+        input.value = task.innerText;
+        editTask.onclick = function () {
+          console.log(task)
+          task.innerText = "";
+          task.append(input.value);
+          editTask.style.display= "none";
+          taskbtn.style.display = "";
+          input.value ="";
+        }
+    
+    }
+    
+})
+
+
+
+
+// let delbtns = document.querySelectorAll(".del");
+// for(btn of delbtns){
+//         btn.addEventListener("click", function (){
+//             let par = this.parentElement;
+//             console.log(par);
+//         });
+// }
